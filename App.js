a764409a2a1a7ddc6220dc3c5f221b9f9b8a7837
Maxine Lien <m_lien@u.pacific.edu>
2019-04-11 17:22:38 -0700

    added basic chat menu screen

--- App.js
+++ App.js
@@ -10,14 +10,15 @@ import Login from './app/components/Login';
 import Edit_Profile from './app/components/Edit_Profile';
 import Home from './app/components/Home';
 import Register from './app/components/Register';
+import Chat_Main from './app/components/Chat_Main';
 
-const AppStack = createStackNavigator({ Home: Home });
+const AppStack = createStackNavigator({ Home: Home, Chat_Main: Chat_Main });
 const AuthStack = createStackNavigator({ Login: Login, Register: Register, Profile: Edit_Profile });
 
 const DrawerNav = createDrawerNavigator({
     AppStack: AppStack,
-  Logout: AuthStack,
-  Profile: Edit_Profile,
+    Logout: AuthStack,
+    Profile: Edit_Profile,
 })
 
 export default createAppContainer(createSwitchNavigator(
@@ -29,28 +30,6 @@ export default createAppContainer(createSwitchNavigator(
     initialRouteName: 'Auth',
   }
 ));
-
-// const NavigationApp = createStackNavigator(
-//     {
-//         Login: { screen: Login, navigationOptions: ({header:null}) },
-//         Edit_Profile: { screen: Edit_Profile, navigationOptions: ({header:null})},
-//         Home: {
-//             screen: Home,
-//             navigationOptions: ({ navigation }) => ({
-//                 title: "RoomMatch",
-//                 headerLeft: <Button navigate={this.props.navigation.toggleDrawer()} title= "=" />
-//             })
-//         },
-//         Register: { screen: Register }
-//     },
-//
-// );
-
-
-
-// const AppContainer = createAppContainer(NavigationApp);
-//
-// export default AppContainer;
 
 const styles = StyleSheet.create({
   container: {
