184aeb8aad6a31b8a6e74c780fadbcb085f2495b
Maxine Lien <m_lien@u.pacific.edu>
2019-04-16 18:45:00 -0700

    profile formatting and small home editing. home still needs fixing

--- app/components/Edit_Profile.js
+++ app/components/Edit_Profile.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 var t = require('tcomb-form-native');
+var v = require('tcomb-validation');
 import {Platform,
         StyleSheet,
         Text,
@@ -18,6 +19,8 @@ import theme from '../theme';
 
 var Form = t.form.Form;
 
+// overriding the text color
+//Form.stylesheet.controlLabel.normal.color = '#fff';
 var Gender = t.enums({
   M: 'Male',
   F: 'Female'
@@ -39,14 +42,14 @@ var Campus = t.enums({
 const tags=['Extrovert', 'Introvert', 'Clean/Tidy', 'Messy', 'Drinks Alcohol', 'Smokes Weed', 'Smokes cigs', 'Night Owl', 'Early Bird']
 
 var Profile = t.struct({
-  Name: t.String,              // a required string
-  Surname: t.maybe(t.String),  // an optional string
+  'First Name': t.String,              // a required string
+  'Last Name': t.maybe(t.String),  // an optional string
   Age: t.Number,               // a required number
   gender: Gender,
   class: Class,
   major: t.String,
   'Im looking for housing...': Campus,
-  'Short Bio': t.maybe(t.String)
+  'Short Bio': t.String
 });
 var options = {
     fields: {
@@ -62,34 +65,46 @@ export default class Edit_Profile extends React.Component {
     }
 
     handlePress(){
+      // call getValue() to get the values of the form
+      var value = this.refs.form.getValue();
+      if (value) {
         this.props.navigation.navigate('Home')
+      }
     }
 
-    static navigationOptions = {
-        header: null,
-    };
+  static navigationOptions = ({navigation, navigationOptions}) => {
+    return {
+      headerTitle: 'Profile',
+      headerStyle: {
+        backgroundColor: '#2b5876',
+      },
+    }
+  }
 
   render() {
     return (
-      <LinearGradient colors={['#2b5876', '#4e4376']}
-        locations={[0,0.8]} style={styles.container}>
-      <ScrollView style={styles.container}>
-        {/* display */}
-        <Form
-          ref="form"
-          type={Profile}
-          options={options}
-        />
-        <TagsView
-            all={tags}
-            //selected={['hi']}
-            isExclusive={false}
-        />
-        <TouchableHighlight style={styles.button} onPress={() => this.handlePress()} underlayColor='#99d9f4'>
-          <Text style={{fontFamily: 'Avenir', color: '#fff', fontSize: 18}}>Save</Text>
-        </TouchableHighlight>
-      </ScrollView>
-      </LinearGradient>
+      <View style={styles.wrapper}>
+        <LinearGradient colors={['#2b5876', '#4e4376']}
+        locations={[0,0.8]} style={styles.header}>
+          <Text style={styles.text}> Profile </Text>
+        </LinearGradient>
+        <ScrollView style={styles.container}>
+          {/* display */}
+          <Form
+            ref="form"
+            type={Profile}
+            options={options}
+          />
+          <TagsView
+              all={tags}
+              selected={[]}
+              isExclusive={false}
+          />
+          <TouchableHighlight style={styles.button} onPress={() => this.handlePress()} underlayColor='#99d9f4'>
+            <Text style={{fontFamily: 'Avenir', color: '#fff', fontSize: 18}}>Save</Text>
+          </TouchableHighlight>
+        </ScrollView>
+      </View>
     );
   }
 
@@ -97,21 +112,33 @@ export default class Edit_Profile extends React.Component {
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
     container: {
-      marginTop: 30,
       flex: 1,
-      //alignItems: 'center',
-      //justifyContent: 'center',
-      padding: 20
+      paddingTop: 20,
+      paddingLeft: 20,
+      paddingRight: 20,
+      backgroundColor: '#fff'
+    },
+    header: {
+      backgroundColor: '#6a7a94',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: 10,
+      height: '10%',
     },
     text: {
-      color: '#6a7a94'
+      color: '#fff',
+      fontSize: 20,
+      fontWeight: 'bold'
     },
     button: {
       alignSelf: 'stretch',
       backgroundColor: theme.primaryColor,
       height: 48,
-      marginBottom: 10,
+      marginBottom: 50,
       justifyContent: 'center',
       alignItems: 'center',
       borderRadius: 30,
