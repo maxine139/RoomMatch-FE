5c5a40fb2132d0e497045c46a3975d65ecf8efb5
Pranav <p_thirunavukkarasu@u.pacific.edu>
2019-05-01 17:56:59 -0700

    Added SocketIO test

--- app/components/Home.js
+++ app/components/Home.js
@@ -12,11 +12,17 @@ import Icon from 'react-native-vector-icons/FontAwesome5'
 import * as profilesServices from '../services/profiles';
 import * as matchesServices from '../services/matches';
 
+import SocketIO from 'socket.io-client';
+
 const cards = []
 
 class Card extends React.Component {
   constructor(props) {
     super(props);
+
+
+    // Creating the socket-client instance will automatically connect to the server.
+    this.socket = SocketIO('http://18.222.255.4:3000');
   }
   render() {
     const name = this.props.firstname + ' ' + this.props.lastname;
